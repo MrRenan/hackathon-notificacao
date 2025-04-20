f7e3d3dd46f5cb3e932d0a36b291cd2bc4a87166
SamuelXIsidorio <samuelisidorio2@gmail.com>
2025-04-20 19:54:15 -0300

    Initial commit - projeto notificacao

---
+++ src/main/java/br/com/fiap/hackathon/notificacao/config/RabbitMQConfig.java
@@ -0,0 +1,13 @@
+package br.com.fiap.hackathon.notificacao.config;
+
+import org.springframework.amqp.core.Queue;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RabbitMQConfig {
+    @Bean
+    public Queue queue() {
+        return new Queue("vacina.aplicada", false);
+    }
+}
